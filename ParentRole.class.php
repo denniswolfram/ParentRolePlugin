a3608dd3c5eab6851a1e2ae0c899bd46aafd602e
Dennis Wolfram <post@dennis-wolfram.de>
2025-04-25 09:49:59 +0000

    Fehler 400

--- ParentRole.class.php
+++ ParentRole.class.php
@@ -14,7 +14,7 @@ class ParentRole extends StudIPPlugin implements SystemPlugin
         if ($GLOBALS['perm']->have_perm('root')) {
             $nav = new Navigation(
                 'Elternverwaltung',
-                PluginEngine::getURL($this, [], 'parentrole/admin')
+                PluginEngine::getURL($this, [], '')
             );
             Navigation::addItem('/admin/config/parentrole', $nav);
         }
